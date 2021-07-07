589a03234cb0e5720c3b2fe21f80ec535b947a1c
NotaCSstudent <mahmudbabu94@gmail.com>
2021-07-07 16:27:17 -0400

    Add files via upload
    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import IPython.display as ipd
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa
+from tqdm import tqdm
+
+
+print("Hello World")
+
